783a08b60215d25da39887aba1b34529bbc006f5
kekoawong <kwong6@nd.edu>
2019-04-24 23:09:42 -0400

    added UI

--- NLP_UI.py
+++ NLP_UI.py
@@ -1,10 +1,49 @@
 
 
 import tkinter
+from tkinter import *
+##from NLPCodeFinal.py import get_tweets(user, query)
+import NLPCodeFinal as fl
 
 gui = tkinter.Tk()
+global var2
 
-B = tkinter.Button(gui, text ="Generate Text")
+E1 = Entry(gui)
+sentence = ''
+def generate():
+    subject = E1.get()
+    global sentence
+    sentence = fl.final_tweet(var2, subject)
+    text.insert(INSERT, sentence)
 
-B.pack()
+def tweet():
+    subject = E1.get();
+    fl.tweet_generated(sentence[:280])
+
+gen_tweet = tkinter.Button(gui, text ="Generate Tweet", command = generate)
+tweet = tkinter.Button(gui, text = "Tweet", command = tweet)
+
+text = tkinter.Text(gui)
+
+L1 = tkinter.Label(gui, text="User: ")
+
+var2 = IntVar()
+
+def user_or():
+    print('fuck')
+
+R_User = Radiobutton(gui, text="User", variable=var2, value=0, command=user_or)
+R_Hashtag = Radiobutton(gui, text="Hashtag", variable=var2, value=1, command=user_or)
+
+
+
+## pack UI widgets
+E1.pack(side = RIGHT)
+L1.pack(side = RIGHT)
+R_User.pack(anchor = E)
+R_Hashtag.pack(anchor = E)
+text.pack()
+gen_tweet.pack()
+tweet.pack()
+
 gui.mainloop()
